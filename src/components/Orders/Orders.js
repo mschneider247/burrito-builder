83cd01a8957f5a26d90bbd5516b7d5d3ae049d88
Michael <mschneider247@gmail.com>
2019-11-06 09:38:33 -0700

    Remove redux from App and move functionality to Orders

--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -1,8 +1,21 @@
 import React from 'react';
+import { Component } from 'react';
 import './Orders.css';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { setOrders } from '../../actions';
+import { getOrders } from '../../apiCalls'
 
-const Orders = props => {
-  const orderEls = props.orders.map(order => {
+export class Orders extends Component {
+
+  componentDidMount() {
+    getOrders()
+      .then(data => this.props.setOrders(data.orders))
+      .catch(err => console.error('Error fetching:', err));
+  }
+
+  orderEls = () => this.props.orders.map(order => {
+    console.log("inside orderEls order", order)
     return (
       <div className="order">
         <h3>{order.name}</h3>
@@ -15,11 +28,25 @@ const Orders = props => {
     )
   });
 
-  return (
-    <section>
-      { orderEls.length ? orderEls : <p>No orders yet!</p> }
-    </section>
-  )
+  render() {
+    let orders = this.orderEls()
+    let ordersLength = orders.length;
+    return (
+      <section>
+        { ordersLength > 0 ? orders : <p>No orders yet!</p> }
+      </section>
+    )
+  }
 }
 
-export default Orders;
+export const mapStateToProps = ({ orders }) => ({
+  orders
+});
+
+export const mapDispatchToProps = dispatch => (
+  bindActionCreators({
+    setOrders,
+  }, dispatch)
+);
+
+export default connect(mapStateToProps, mapDispatchToProps)(Orders);
