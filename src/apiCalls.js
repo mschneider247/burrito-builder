138d29893d1698a9dafa2d4ffe4292197dc4cbec
Michael <mschneider247@gmail.com>
2019-11-06 09:56:16 -0700

    Create new apiCall for posting orders

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -2,3 +2,13 @@ export const getOrders = () => {
   return fetch('http://localhost:3001/api/v1/orders')
       .then(response => response.json())
 }
+
+export const postOrder = (order) => {
+  const options = {
+    method: 'POST',
+    body: JSON.stringify(order),
+    headers: {'Content-Type': 'application/json'}
+  }
+  return fetch('http://localhost:3001/api/v1/orders', options)
+    .then(response => response.json())
+}
