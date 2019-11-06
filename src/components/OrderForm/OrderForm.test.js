da6140b8f4944bcf3c0b70111e9f5d1bead9ce45
Michael <mschneider247@gmail.com>
2019-11-06 11:16:05 -0700

    Snapshot test and two methods tests for OrderForm

---
+++ src/components/OrderForm/OrderForm.test.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import OrderForm from './OrderForm'
+import { postOrder } from '../../apiCalls'
+
+describe('OrderForm component', () => {
+  const wrapper = shallow(<OrderForm/>)
+  it('Should match its snapshot', () => {
+    expect(wrapper).toMatchSnapshot()
+  });
+
+  it('Should update state with name changes when handleNameChange is called', () => {
+    const expectedState = {name: 'Danny', ingredients: []}
+    const event = {target: {name: 'name', value: 'Danny'}};
+    wrapper.instance().handleNameChange(event)
+    expect(wrapper.state()).toEqual(expectedState)
+  })
+
+  it('Should update state with ingredient when handleIngredientChange is called', () => {
+    const expectedState = {name: 'Danny', ingredients: ['cheese']}
+    const event = {target: {name: 'cheese'}, preventDefault: jest.fn()};
+    wrapper.instance().handleIngredientChange(event)
+    expect(wrapper.state()).toEqual(expectedState)
+  })
+})
